a363fc64a9b3089df226be7d8ef63f10bf3cfc12
Joe Conway <mail@joeconway.com>
2025-11-11 17:09:21 -0500

    Update extension version to 2.0
    Update extension version to 2.0 even though there are no user facing
    changes. This is to ensure users upgrading from previous versions on
    other forks can move to this one via ALTER EXTENSION...UPDATE TO...

---
+++ pgnodemx--1.4--2.0.sql
@@ -0,0 +1,6 @@
+/* contrib/pgnodemx/pgnodemx--1.4--2.0.sql */
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pgnodemx" to load this file. \quit
+
+-- noop --
