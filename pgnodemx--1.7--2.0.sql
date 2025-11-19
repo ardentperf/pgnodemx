212ebdf2c028a0abbd3ca79e0284bedafa7b5eb3
Joe Conway <mail@joeconway.com>
2025-11-18 21:06:40 -0500

    Add a v1.7 to 2.0 upgrade path
    pgnodemx upgrades from v1.4 through 2.0 are all noops from the
    standpoint of the sql files, but add a path from 1.7 since that
    was the most recent version presented by the Crunchy repo.

---
+++ pgnodemx--1.7--2.0.sql
@@ -0,0 +1,6 @@
+/* contrib/pgnodemx/pgnodemx--1.7--2.0.sql */
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pgnodemx" to load this file. \quit
+
+-- noop --
